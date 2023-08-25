05801f8fe64d937c8a9d4df4abb209ecdc79e7af
KlintonICT <chhunklinton91@gmail.com>
2023-08-25 21:34:46 +0700

    create unit tests for auth router handlers

--- auth/src/test/setup.ts
+++ auth/src/test/setup.ts
@@ -1,10 +1,18 @@
 import { MongoMemoryServer } from 'mongodb-memory-server';
 import mongoose from 'mongoose';
+import request from 'supertest';
+
 import { app } from '../app';
+
+declare global {
+  var signin: () => Promise<string[]>;
+}
 
 let mongo: any;
 
 beforeAll(async () => {
+  process.env.JWT_KEY = 'asdfasdf';
+
   mongo = await MongoMemoryServer.create();
   const mongoUri = mongo.getUri();
 
@@ -26,3 +34,17 @@ afterAll(async () => {
 
   await mongoose.connection.close();
 });
+
+global.signin = async () => {
+  const email = 'test@test.com';
+  const password = 'password';
+
+  const res = await request(app)
+    .post('/api/users/signup')
+    .send({ email, password })
+    .expect(201);
+
+  const cookie = res.get('Set-Cookie');
+
+  return cookie;
+};
