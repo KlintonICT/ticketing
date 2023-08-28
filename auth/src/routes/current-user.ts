b5c483aca8b1012b89a570f55340bf88e6cd6dfa
KlintonICT <chhunklinton91@gmail.com>
2023-08-28 17:08:43 +0700

    update auth service to use ticketing common package

--- auth/src/routes/current-user.ts
+++ auth/src/routes/current-user.ts
@@ -1,6 +1,6 @@
 import express from 'express';
 
-import { currentUser } from '../middlewares/current-user';
+import { currentUser } from '@klinton-org/ticketing-common';
 
 const router = express.Router();
 
