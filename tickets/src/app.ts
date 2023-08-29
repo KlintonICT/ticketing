30be85f391e0e3b85473663876a0b1ccc45388a4
KlintonICT <chhunklinton91@gmail.com>
2023-08-29 10:18:16 +0700

    create creating a new ticket route handler

--- tickets/src/app.ts
+++ tickets/src/app.ts
@@ -2,7 +2,13 @@ import express from 'express';
 import 'express-async-errors';
 import { json } from 'body-parser';
 import cookieSession from 'cookie-session';
-import { errorHandler, NotFoundError } from '@klinton-org/ticketing-common';
+import {
+  errorHandler,
+  NotFoundError,
+  currentUser,
+} from '@klinton-org/ticketing-common';
+
+import { createTicketRouter } from './routes/new';
 
 const app = express();
 
@@ -14,6 +20,10 @@ app.use(
     secure: process.env.NODE_ENV !== 'test',
   })
 );
+
+app.use(currentUser);
+
+app.use(createTicketRouter);
 
 app.all('*', async () => {
   throw new NotFoundError();
--- tickets/src/test/setup.ts
+++ tickets/src/test/setup.ts
@@ -1,11 +1,9 @@
 import { MongoMemoryServer } from 'mongodb-memory-server';
 import mongoose from 'mongoose';
-import request from 'supertest';
-
-import { app } from '../app';
+import jwt from 'jsonwebtoken';
 
 declare global {
-  var signin: () => Promise<string[]>;
+  var signin: () => string[];
 }
 
 let mongo: any;
@@ -35,16 +33,15 @@ afterAll(async () => {
   await mongoose.connection.close();
 });
 
-global.signin = async () => {
-  const email = 'test@test.com';
-  const password = 'password';
+global.signin = () => {
+  const payload = {
+    id: 'lsdkf394dsf83',
+    email: 'test@test.com',
+  };
+  const token = jwt.sign(payload, process.env.JWT_KEY!);
+  const session = { jwt: token };
+  const sessionJSON = JSON.stringify(session);
+  const base64 = Buffer.from(sessionJSON).toString('base64');
 
-  const res = await request(app)
-    .post('/api/users/signup')
-    .send({ email, password })
-    .expect(201);
-
-  const cookie = res.get('Set-Cookie');
-
-  return cookie;
+  return [`session=${base64}`];
 };
