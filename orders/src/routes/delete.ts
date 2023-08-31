c40f57bb72703491067445437e64ee270306d63c
KlintonICT <chhunklinton91@gmail.com>
2023-08-31 22:47:39 +0700

    create orders service

---
+++ orders/src/routes/delete.ts
@@ -0,0 +1,36 @@
+import express, { Request, Response } from 'express';
+import {
+  NotAuthenticatedError,
+  NotFoundError,
+  requireAuth,
+} from '@klinton-org/ticketing-common';
+
+import { Order, OrderStatus } from '../models/order';
+
+const router = express.Router();
+
+router.delete(
+  '/api/orders/:orderId',
+  requireAuth,
+  async (req: Request, res: Response) => {
+    const { orderId } = req.params;
+
+    const order = await Order.findById(orderId);
+
+    if (!order) {
+      throw new NotFoundError();
+    }
+    if (order.userId !== req.currentUser!.id) {
+      throw new NotAuthenticatedError();
+    }
+
+    order.status = OrderStatus.Cancelled;
+    await order.save();
+
+    // publishing an event saying this wa cancelled!
+
+    res.status(204).send(order);
+  }
+);
+
+export { router as deleteOrderRouter };
